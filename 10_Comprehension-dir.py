e37ed0036540d1a2b7227d15cd496db8ec7b1c02
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-09-04 16:24:38 +0530

    Day 03

--- 10_Comprehension-dir.py
+++ 10_Comprehension-dir.py
@@ -1,3 +1,20 @@
-a= "Test"
+a= 10
 print(dir(a))
 print([member for member in dir(a) if member.startswith("_") == False])
+
+def Test():
+    print("Test")
+
+print(type(Test))
+'''
+class Test
+{
+    void operator()(int data)
+};
+
+t1 = Test()
+t1(10)  <-- Function objects/Functors/Predicates
+'''
+
+# Filter only the callable members from the non-dunder members' list
+# Hint: callable(), getattr()
